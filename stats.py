1f390d1902f27577aac2ac076f08a0233229277c
GeorgeFritz8 <30572@ma-web.nl>
2020-09-16 15:27:57 +0200

    stats

---
+++ stats.py
@@ -0,0 +1,37 @@
+name = "George Fritz"
+codeName = "Leopard"
+FG = "Media College"
+LV = 80
+R = "Mage"
+STR = "{O}{O}{O}{ }{ }"
+HP = "{O}{O}{O}{ }{ }"
+DF = "{O}{O}{ }{ }{ }"
+AaE = "{O}{O}{O}{O}{O}"
+M = "{O}{O}{O}{O}{O}"
+WaT = "Healing wand, Book of advanced spells, Dragontooth Dagger, Bow of the wanderer."
+N = "Passive until provoked, always hostile towards opposing factions."
+SL = "Spawn Location: Any turf owned by the Media College faction."
+DLS = "{O}{O}{O}{ }{ }"
+DLG = "{O}{O}{O}{O}{O}"
+
+
+
+
+
+
+
+print("Name:", name)
+print("Code Name:", codeName)
+print("Faction Group:", FG)
+print("Level:", LV)
+print("Role:", R)
+print("Strength:", STR)
+print("Health:", HP)
+print("Defence:", DF)
+print("Agility and Evasiveness:", AaE)
+print("Magic:", M)
+print("Weapons and/or Tools:", WaT)
+print("Nature:", N)
+print("Spawn Location:", SL)
+print("Danger level (Solo):", DLS)
+print("Danger level (In group fight):", DLG)

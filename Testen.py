5cb45e0c605256a938ed406c02b338e031c9054c
GeorgeFritz8 <30572@ma-web.nl>
2020-09-23 14:13:42 +0200

    Output.py Succesvol gemaakt en verstuurd

---
+++ Testen.py
@@ -0,0 +1,7 @@
+import random
+
+willekeur = random.randrange(1, 11)
+#print(willekeur)
+
+if (input() > willekeur):
+    print("lager")
---
+++ output.py
@@ -0,0 +1,109 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> Microsoft Windows [Version 10.0.18363.1082]
+(c) 2019 Microsoft Corporation. Alle rechten voorbehouden.
+
+C:\Users\georg>py
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> help
+Type help() for interactive help, or help(object) for help about object.
+>>> "help"
+'help'
+>>> help()
+
+Welcome to Python 3.8's help utility!
+
+If this is your first time using Python, you should definitely check out
+the tutorial on the Internet at https://docs.python.org/3.8/tutorial/.
+
+Enter the name of any module, keyword, or topic to get help on writing
+Python programs and using Python modules.  To quit this help utility and
+return to the interpreter, just type "quit".
+
+To get a list of available modules, keywords, symbols, or topics, type
+"modules", "keywords", "symbols", or "topics".  Each module also comes
+with a one-line summary of what it does; to list the modules whose name
+or summary contain a given string such as "spam", type "modules spam".
+
+help> help(object)
+No Python documentation found for 'help(object)'.
+Use help() to get the interactive help utility.
+Use help(str) for help on the str class.
+
+help> copyright
+Help on _Printer in module _sitebuiltins object:
+
+copyright = class _Printer(builtins.object)
+ |  copyright(name, data, files=(), dirs=())
+ |
+ |  interactive prompt objects for printing the license text, a list of
+ |  contributors and the copyright notice.
+ |
+ |  Methods defined here:
+ |
+ |  __call__(self)
+ |      Call self as a function.
+ |
+ |  __init__(self, name, data, files=(), dirs=())
+ |      Initialize self.  See help(type(self)) for accurate signature.
+ |
+ |  __repr__(self)
+ |      Return repr(self).
+ |
+ |  ----------------------------------------------------------------------
+ |  Data descriptors defined here:
+ |
+ |  __dict__
+ |      dictionary for instance variables (if defined)
+ |
+ |  __weakref__
+ |      list of weak references to the object (if defined)
+ |
+ |  ----------------------------------------------------------------------
+ |  Data and other attributes defined here:
+ |
+ |  MAXLINES = 23
+
+help>
+
+You are now leaving help and returning to the Python interpreter.
+If you want to ask for help on a particular object directly from the
+interpreter, you can type "help(object)".  Executing "help('string')"
+has the same effect as typing a particular string at the help> prompt.
+>>>
+>>> credits
+    Thanks to CWI, CNRI, BeOpen.com, Zope Corporation and a cast of thousands
+    for supporting Python development.  See www.python.org for more information.
+>>>
+>>> license
+Type license() to see the full license text
+>>> license()
+A. HISTORY OF THE SOFTWARE
+==========================
+
+Python was created in the early 1990s by Guido van Rossum at Stichting
+Mathematisch Centrum (CWI, see http://www.cwi.nl) in the Netherlands
+as a successor of a language called ABC.  Guido remains Python's
+principal author, although it includes many contributions from others.
+
+In 1995, Guido continued his work on Python at the Corporation for
+National Research Initiatives (CNRI, see http://www.cnri.reston.va.us)
+in Reston, Virginia where he released several versions of the
+software.
+
+In May 2000, Guido and the Python core development team moved to
+BeOpen.com to form the BeOpen PythonLabs team.  In October of the same
+year, the PythonLabs team moved to Digital Creations, which became
+Zope Corporation.  In 2001, the Python Software Foundation (PSF, see
+https://www.python.org/psf/) was formed, a non-profit organization
+created specifically to own Python-related Intellectual Property.
+Zope Corporation was a sponsoring member of the PSF.
+
+All Python releases are Open Source (see http://www.opensource.org for
+the Open Source Definition).  Historically, most, but not all, Python
+Hit Return for more, or q (and Return) to quit: q
+>>>
+
+SyntaxError: invalid syntax
+>>> 
